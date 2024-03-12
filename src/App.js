064a288cec8f8a40abb1e64164a0fd3d71ae342a
yungshun317 <seraph317@gmail.com>
2024-03-12 23:16:13 +0800

    [FEAT] Render the item list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+const initialItems = [
+  {
+    id: 1, description: "Passports", quantity: 2, packed: false
+  },
+  {
+    id: 2, description: "Socks", quantity: 12, packed: true
+  },
+  {
+    id: 3, description: "Charger", quantity: 1, packed: false
+  }
+]
+
 export default function App() {
   return <div className="app">
     <Logo />
@@ -20,11 +32,28 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>
+  return <div className="list">
+    <ul>
+      {initialItems.map(item => (
+          <Item item={item} />
+      ))}
+    </ul>
+  </div>
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>&times;</button>
+    </li>
+  );
 }
 
 function Stats() {
-  return <footer>
+  return <footer className="stats">
     <em>You have X items in your list, and you already packed X (X%).</em>
   </footer>
 }
