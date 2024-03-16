31ecc6e811ecfaf743c315e269ae12a42fc29a31
yungshun317 <seraph317@gmail.com>
2024-03-16 16:00:10 +0800

    [FEAT] Instruct the browser to display a dialog with an optional message and to wait until the user either confirms or cancels the dialog with `window.confirm`

--- src/App.js
+++ src/App.js
@@ -33,7 +33,9 @@ export default function App() {
   }
 
   function handleClearList() {
-      setItems([]);
+      const confirmed = window.confirm("Are you sure you want to delete all items?")
+
+      if (confirmed) setItems([]);
   }
 
   return <div className="app">
